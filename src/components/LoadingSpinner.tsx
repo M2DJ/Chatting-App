8381063220f5741d0fc3697f0980ba32047ae972
M2DJ Gaming <m2dj2004@gmail.com>
2026-06-22 14:55:42 +0300

    Added an animation curve to the loading spinner

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,20 +1,23 @@
 interface LoadingProps {
-  size: "small" | "medium" | "large" | 'xl';
-  color: string;
+  size: "small" | "medium" | "large" | "xl";
+  color?: string;
 }
 
-const LoadingSpinner = ({ size, color = '000000' }: LoadingProps) => {
+const LoadingSpinner = ({
+  size = "medium",
+  color = "000000",
+}: LoadingProps) => {
   const sizeClasses: Record<LoadingProps["size"], string> = {
     small: "h-6 w-6 border-2",
     medium: "h-10 w-10 border-3",
     large: "h-16 w-16 border-4",
-    xl: "h-24 w-24 border-5"
+    xl: "h-24 w-24 border-5",
   };
 
   return (
     <div
-      className={`${sizeClasses[size]} border-[#${color}] border-t-transparent rounded-full animate-spin`}
-    ></div>
+      className={`${sizeClasses[size]} border-[#${color}] border-r-transparent rounded-full animate-spin [animation-timing-function:ease-in-out]`}
+    />
   );
 };
 
