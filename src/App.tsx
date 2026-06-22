bed93514cd821e592a0dda31b522cb390ba19504
M2DJ Gaming <m2dj2004@gmail.com>
2026-06-22 15:03:52 +0300

    Added the routes to the app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import ChatScreen from "./pages/ChatScreen";
 
 function App() {
   return (
     <>
-          
+    <Routes>
+      <Route element={<LoginPage />} path="/login"/>
+      <Route element={<SignUpPage/>} path="/signup" />
+      <Route element={<ChatScreen/>} path="/chat"/>
+    </Routes>      
     </>
   );
 }
