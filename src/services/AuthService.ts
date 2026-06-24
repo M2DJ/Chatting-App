4ae9008fc746ef6b7c0f189c12daf2d69cbcede1
M2DJ Gaming <m2dj2004@gmail.com>
2026-06-24 15:29:22 +0300

    Added auth services for logging in and signing up with email and password and logging out

---
+++ src/services/AuthService.ts
@@ -0,0 +1,51 @@
+import { UNSAFE_ErrorResponseImpl } from "react-router-dom";
+import { supabase } from "./SupabaseClient";
+
+class SupabaseAuthService {
+  async signUpWithEmail(email: string, password: string) {
+    try {
+      const { data, error } = await supabase.auth.signUp({
+        email: email,
+        password: password,
+      });
+
+      if (error) throw error;
+
+      return { success: true, data: data, error: null };
+    } catch (e: any) {
+      console.error("Error Signing up with email: ", e);
+      return { success: false, data: null, error: e.message };
+    }
+  }
+
+  async loginWithEmail(email: string, password: string){
+    try{
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password,
+        })
+
+        if(error) throw error;
+
+        return { success: true, data: data, error: null };
+    } catch(e: any) {
+        console.error("Error logging in: ", e);
+        return { success: false, data: null, error: e.message};
+    }
+  }
+
+  async logout() {
+    try{
+        const { error } = await supabase.auth.signOut();
+
+        if(error) throw error;
+
+        return { success: true, data: null, error: null};
+    } catch(e: any) {
+        console.error("Error signing out: ", e);
+        return { success: false, data: null, error: e.message};
+    }
+  }
+}
+
+export const authService = new SupabaseAuthService();
