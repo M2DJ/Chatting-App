214aaaca7845c46cf349f15d47629ded88948734
M2DJ Gaming <m2dj2004@gmail.com>
2026-06-28 02:53:07 +0300

    Created the functions for updating the user password

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -17,36 +17,64 @@ class SupabaseAuthService {
     }
   }
 
-  async loginWithEmail(email: string, password: string){
-    try{
-        const { data, error } = await supabase.auth.signInWithPassword({
-            email: email,
-            password: password,
-        })
+  async loginWithEmail(email: string, password: string) {
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: password,
+      });
 
-        if(error) throw error;
+      if (error) throw error;
 
-        return { success: true, data: data, error: null };
-    } catch(e: any) {
-        console.error("Error logging in: ", e);
-        return { success: false, data: null, error: e.message};
+      return { success: true, data: data, error: null };
+    } catch (e: any) {
+      console.error("Error logging in: ", e);
+      return { success: false, data: null, error: e.message };
     }
   }
 
   async logout() {
-    try{
-        const { error } = await supabase.auth.signOut();
+    try {
+      const { error } = await supabase.auth.signOut();
 
-        if(error) throw error;
+      if (error) throw error;
 
-        return { success: true, data: null, error: null};
-    } catch(e: any) {
-        console.error("Error signing out: ", e);
-        return { success: false, data: null, error: e.message};
+      return { success: true, data: null, error: null };
+    } catch (e: any) {
+      console.error("Error signing out: ", e);
+      return { success: false, data: null, error: e.message };
     }
   }
 
-//   async sendPasswordResetEmail(email: string)
+  async sendPasswordResetEmail(email: string, redirectTo: string) {
+    try {
+      const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
+        redirectTo: redirectTo,
+      });
+
+      if (error) throw error;
+
+      return { success: true, data: data, error: null };
+    } catch (e: any) {
+      console.error("Error sending reset email: ", e);
+      return { success: false, data: null, error: e.message };
+    }
+  }
+
+  async updateUserPassword(newPassword: string) {
+    try {
+      const { data, error } = await supabase.auth.updateUser({
+        password: newPassword,
+      });
+
+      if (error) throw error;
+
+      return { success: true, data: data, error: null };
+    } catch (e: any) {
+      console.error("Error updating user password: ", e);
+      return { success: false, data: null, error: e.message };
+    }
+  }
 }
 
 export const authService = new SupabaseAuthService();
