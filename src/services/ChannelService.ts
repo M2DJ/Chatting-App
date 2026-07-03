f9ad07235034a028e9c45d68983fa13c33558144
M2DJ Gaming <m2dj2004@gmail.com>
2026-07-03 16:01:37 +0300

    Created the search function in the ChannelService file

--- src/services/ChannelService.ts
+++ src/services/ChannelService.ts
@@ -1,7 +1,25 @@
-class ChannelService{
-    async lookUpUsers(userName: string){
-        
+import { supabase } from "./SupabaseClient";
+
+class ChannelService {
+  async lookUpUsers(userName: string) {
+    const isEmail = userName.includes("@") && userName.includes('.');
+
+    try {
+      const { data, error } = await supabase
+        .from("usersPublic")
+        .select("user_email, user_name")
+        .ilike(isEmail ? "user_email" : "user_name", `%${userName}%`);
+
+        if(error) {
+            return { success: false, data: null, error: error.message };
+        }
+
+        return { success: true, data: data, error: null };
+    } catch (e: any) {
+        console.error('Error looking up user: ', e);
+        return { success: false, data: null, error: e.message };
     }
+  }
 }
 
 export const channelService = new ChannelService();
